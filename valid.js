68f4413183e0e8f9d7cb8a2aefb744f00de8dd83
kanivaradhan <155565678+kanivaradhan@users.noreply.github.com>
2024-01-03 22:12:01 +0530

    Add files via upload

---
+++ valid.js
@@ -0,0 +1,37 @@
+var email= document.forms['form']['email'];
+var password= document.forms['form']['password'];
+var email_error= document.getElementById("email_error");
+var pass_error= document.getElementById("pass_error");
+email.addEventlistener("textInput",email_Verify);
+password.addEventlistener("textInput", pass_Verify);
+function validated(){
+    if (email.value.length < 9) {
+        email.style.border="1px solid red ";
+        email_error.style.display="block";
+        email.focus();
+        return false;
+    }
+    if (password.value.length >= 8) {
+        password.style.border="1px solid red ";
+        pass_error.style.display="block";
+        pass.focus();
+        return false;
+    }
+}
+function email_Verify(){
+    if (email.value.length < 9) {
+        email.style.border="1px solid silver";
+        email_error.style.display="none";
+        email.focus();
+        return true;
+    }
+    
+}
+function pass_Verify(){
+if (password.value.length >=8) {
+    pass.style.border="1px solid silver";
+    pass_error.style.display="none";
+
+    return true;
+}
+}
